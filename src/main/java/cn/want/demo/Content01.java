b234c20c016cf6320efccc384b4ce85216db14ea
Want <bujiejisheng@163.com>
2022-04-15 17:07:33 +0800

    1次更改

---
+++ src/main/java/cn/want/demo/Content01.java
@@ -0,0 +1,9 @@
+package cn.want.demo;
+
+public class Content01 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git，1 in master ");
+
+    }
+}
