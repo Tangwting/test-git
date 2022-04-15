b6fb4ec1d458752563eb94ed63ce37859b47ad42
Want <bujiejisheng@163.com>
2022-04-15 17:08:04 +0800

    2次更改

--- src/main/java/cn/want/demo/Content01.java
+++ src/main/java/cn/want/demo/Content01.java
@@ -4,6 +4,7 @@ public class Content01 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git，1 in master ");
+        System.out.println("hello git，2 in master ");
 
     }
 }
