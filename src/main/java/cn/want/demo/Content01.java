99737b65a85428e2b3837b6eab34558ad8e4e70b
Want <bujiejisheng@163.com>
2022-04-15 17:47:38 +0800

    1次更改for push

--- src/main/java/cn/want/demo/Content01.java
+++ src/main/java/cn/want/demo/Content01.java
@@ -7,6 +7,7 @@ public class Content01 {
         System.out.println("hello git，2 in master ");
         System.out.println("hello git，1 in branch hot-fix-one ");
         System.out.println("hello git，3 in master ");
+        System.out.println("hello git，1 change for push in master ");
 
     }
 }
